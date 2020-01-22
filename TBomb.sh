f3cf02e5897637b3334f46e2dd51d37fa4406d39
x-hacker-mafia-x <45988483+x-hacker-mafia-x@users.noreply.github.com>
2020-01-22 23:12:27 +0530

    Add files via upload

---
+++ TBomb.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+clear
+echo -e "\e[4;31m MOHIT PRODUCTIONS !!! \e[0m"
+echo -e "\e[1;34m Presents \e[0m"
+echo -e "\e[1;32m FBomb \e[0m"
+echo "Press Enter To Continue"
+read a1
+if [[ -s update.mohit ]];then
+echo "All Requirements Found...."
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install figlet toilet python curl -y
+apt install python3-pip
+pip install -r requirements.txt
+echo This Was Made By MOHIT >update.mohit
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet MOHITBOMB
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border Mohit
+echo -e "\e[4;34m This Was Created By Mohit\e[0m"
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: itsmohitkumarbro@gmail.com \e[0m"
+echo " "
+echo -e "\e[4;31m Please Read Instruction !!! \e[0m"
+echo " "
+echo "Press 1 To  Start SMS Bomber "
+echo "Press 2 To  Start Call Bomber "
+echo "Press 3 To  Update (Works On Linux And Termux) "
+echo "Press 4 To  View Features "
+echo "Press 5 To  Exit "
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 bomber.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By MOHIT'> call.xxx
+python3 bomber.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/TheSpeedX/TBomb
+if [[ -s TBomb/TBomb.sh ]];then
+cd Test
+cp -r -f * .. > temp
+cd ..
+rm -rf  TBomb >> temp
+rm update.mohit >> temp
+rm temp
+chmod +x TBomb.sh
+fi
+echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m All Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Restart..."
+read a6
+./TBomb.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet MOHITBOMB
+echo -e "\e[1;34mCreated By \e[1;34m"
+toilet -f mono12 -F border Mohit
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m"
+echo "  [+] Unlimited And Super-Fast Bombing"
+echo "  [+] International Bombing"
+echo "  [+] Call Bombing "
+echo "  [+] Protection List"
+echo "  [+] Automated Future Updates"
+echo "  [+] Easy To Use And Embed in Code"
+echo -e "\e[1;32m                   Contributors\e[1;33m"
+echo -e "\e[1;33m      [*]  Mohit   \e[1;31m"
+echo "         [-] Mail At: itsmohitsmohitkumarbro@gmail.com"
+echo ""
+echo ""
+echo -e "\e[1;31m This Script is not for educational purpose.\e[0m"
+echo -e "\e[1;31m Do Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m But I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
+echo  " "
+echo -e "\e[4;31m That's All !!!\e[0m"
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: itsmohitkumarbro@gmail.com \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+elif [ $ch -eq 5 ];then
+clear
+echo -e "\e[1;31m"
+figlet MOHITBOMB
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border Mohit
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: itsmohitkumarbro@gmail.com \e[0m"
+echo " "
+exit 0
+else
+echo -e "\e[4;32m Invalid Input !!! \e[0m"
+echo "Press Enter To Go Home"
+read a3
+clear
+fi
+done
